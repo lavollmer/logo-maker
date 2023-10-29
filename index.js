84eb25014712bf64096ba76001c19adef2a7f390
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-10-29 14:38:43 -0500

    fixing the square SVG

--- index.js
+++ index.js
@@ -7,45 +7,6 @@ const inquirer = require("inquirer");
 const { Triangle } = require("./lib/shapes.js");
 const { Square } = require("./lib/shapes.js");
 const { Circle } = require("./lib/shapes.js");
-// const { renderSVG } = require("./lib/shapes");
-
-//asking user prompt questions for logo
-// inquirer.prompt([
-//   {
-//     type: "input",
-//     message: "What text would you like in your logo?",
-//     name: "textLogo"
-//   },
-//   {
-//     type: "list",
-//     message: "What color would you like your logo text to be?",
-//     name: "colorTextLogo",
-//     choices: ["red", "green", "blue"]
-//   },
-//   {
-//     type: "list",
-//     message: "What shape would you like your logo to be?",
-//     name: "shapeLogo",
-//     choices: ["square", "circle", "triangle"]
-//   },
-//   {
-//     type: "list",
-//     message: "What color would you like your logo to be?",
-//     name: "colorLogo",
-//     choices: ["red", "green", "blue"]
-//   }
-// ]).then(answers => {
-//   console.log(answers);
-//   return answers;
-// })
-
-
-// give error if doesn't run
-// function writeFile(answers) {
-//   //write the logosvg file
-//   fs.writeFile('logo.svg', answers, (err) =>
-//     err ? console.error(err) : console.log('Done!'))
-// }
 
 // intiatilizes the write file with SVG
 function init() {
@@ -80,13 +41,13 @@ function init() {
     let shape;
     switch (answers.shapeLogo) {
       case "square":
-        shape = new Square(answers.colorLogo, answers.textLogo);
+        shape = new Square(answers.colorLogo, answers.textLogo, answers.colorTextLogo, answers.shapeLogo);
         break;
       case "circle":
-        shape = new Circle(answers.colorLogo, answers.textLogo);
+        shape = new Circle(answers.colorLogo, answers.textLogo, answers.colorTextLogo, answers.shapeLogo);
         break;
       default:
-        shape = new Triangle(answers.colorLogo, answers.textLogo);
+        shape = new Triangle(answers.colorLogo, answers.textLogo, answers.colorTextLogo, answers.shapeLogo);
         break;
     }
     let myOutput = shape.renderSVG();
